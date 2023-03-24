dc09a10308ead9fea12a25531e2180948f2a0a15
jollyjiyoun <0715jiyounkim@gmail.com>
2023-03-24 15:12:36 +0900

    Run Commands

---
+++ commands/run.sh
@@ -0,0 +1,7 @@
+python ../main.py \
+--source_path PATH_TO_MIMIC3 \
+--input_path PREPROCESS_OUTPUT_PATH \ 
+--output_path MODELING_OUTPUT_PATH \
+--wandb_entity_name ENTITY \
+--wandb_project_name PROJECT \
+--preprocess
